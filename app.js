1e2fd4b9b940df32803451df2362645b78544a7d
yejoon90417 <yejoon90417@gmail.com>
2026-04-05 21:52:24 +0900

    Render camera through canvas and video fallback

--- app.js
+++ app.js
@@ -584,6 +584,14 @@ function syncEffectPlayback(isVisible) {
   }
 }
 
+function drawBackground(width, height) {
+  ctx.save();
+  ctx.translate(width, 0);
+  ctx.scale(-1, 1);
+  ctx.drawImage(dom.video, 0, 0, width, height);
+  ctx.restore();
+}
+
 function ensureEffectCanvas(workWidth, workHeight) {
   if (
     state.effectCanvas.width !== workWidth
@@ -677,6 +685,7 @@ function drawFrame() {
   const height = window.innerHeight;
   const now = performance.now();
   ctx.clearRect(0, 0, width, height);
+  drawBackground(width, height);
 
   let rightHand = getRightHand(state.latestResults);
   if (rightHand) {
